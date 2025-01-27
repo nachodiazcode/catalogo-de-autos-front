9f150fc2b0e6f4c06dbec2266d6afb9261ca858b
nachodiazcode <ignaciosergiodiaz@gmail.com>
2025-01-26 22:47:02 -0300

    se agrega contacto

---
+++ src/app/catalogo/contacto/page.tsx
@@ -0,0 +1,8 @@
+export default function Contacto() {
+    return (
+      <main className="min-h-screen flex items-center justify-center bg-gray-50">
+        <h1 className="text-4xl font-bold text-gray-800">Página de Contacto</h1>
+      </main>
+    );
+  }
+  
